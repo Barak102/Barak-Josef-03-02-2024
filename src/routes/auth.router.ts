783ba7211996ca895c8ae0969cd5a92c6557a19c
Barak <barak102@gmail.com>
2024-02-04 14:34:38 +0200

    -

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { AuthController } from "../controllers/auth.controller";
 
 const authRouter = express.Router();
+//
 const controller = new AuthController();
 authRouter.post("/register", controller.register);
 
